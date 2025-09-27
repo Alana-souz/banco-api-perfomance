ec2af929836d4316c21353115fae3e3b5e92e8cf
Alana <alanareis10@gmail.com>
2025-09-26 22:42:38 -0300

    Adcionando a funcao de helpe para obtecao de token e criando o teste de transferencia

--- tests/login.test.js
+++ tests/login.test.js
@@ -3,13 +3,13 @@ import { sleep, check } from 'k6';
 const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
 
 export const options = {
-  /*
+  
   stages: [
     { duration: '5s', target: 10 },
     { duration: '20m', target: 10 },
     { duration: '5s', target: 0 },
   ],
-  */
+  
   iterations: 1,
   thresholds: {
     http_req_duration: ['p(90)<3000', 'max<5000'],
